c76590ba2bfa09dc608f51dedb89812795c4f5dd
= <hiagodeoliveiratv@gmail.com>
2021-08-10 09:04:59 -0300

    Final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObjects';
 
 export const search = (req: Request, res: Response) => {
+    let query = req.query.q as string;
 
-    // res.render('pages/search');
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ server.use(mainRoutes);
 // rota não encontrada
 
 server.use((req,res)=>{
-    res.send('página não encontrada.');
+    res.render('pages/404');
 });
 // inicia o servidor
 server.listen(process.env.PORT);
