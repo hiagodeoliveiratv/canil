8a616db8d0b699337d3335d3e9da677cad498627
= <hiagodeoliveiratv@gmail.com>
2021-08-10 07:18:15 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SeachController from '../controllers/searchController';
+
+
+
+
+const router = Router();
+
+// rota home
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', SeachController.search);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+
+// inicia o dotenv pra rodar as variáveis de ambiente
+dotenv.config();
+
+// inicia o servidor...
+const server = express();
+
+// define a engine mustrache pra trabalhar com html
+server.set('view engine', 'mustache');
+
+// seta as pastas de view 
+server.set('views', path.join(__dirname, 'views'));
+
+// seta a engine mustache
+server.engine('mustache', mustache());
+
+// seta a pasta public dos arquivos html e css 
+server.use(express.static(path.join(__dirname, '../public')));
+
+
+// Rotas
+server.use(mainRoutes);
+
+// rota não encontrada
+
+server.use((req,res)=>{
+    res.send('página não encontrada.');
+});
+// inicia o servidor
+server.listen(process.env.PORT);
+
